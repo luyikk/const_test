b4db50d57cbe9d31ff453a7a132b680ea24e36ef
luyikk <547386448@qq.com>
2022-08-04 13:32:10 +0800

    fmt

--- src/const_hex.rs
+++ src/const_hex.rs
@@ -1,37 +1,31 @@
-
-
-pub const fn get_encode_hex_len(len:usize) ->usize{
-    len*2
+pub const fn get_encode_hex_len(len: usize) -> usize {
+    len * 2
 }
 
-pub const fn hex(buf:&'static [u8])->HexData{
-    HexData{
+pub const fn hex(buf: &'static [u8]) -> HexData {
+    HexData {
         buf,
-        len:get_encode_hex_len(buf.len())
+        len: get_encode_hex_len(buf.len()),
     }
 }
 
-
-
-pub struct HexData{
-    pub buf:&'static [u8],
-    pub len:usize
+pub struct HexData {
+    pub buf: &'static [u8],
+    pub len: usize,
 }
 
-impl HexData{
-    pub const fn encode<const LEN:usize>(mut input:&[u8])->[u8;LEN]{
-        let mut out=[0;LEN];
+impl HexData {
+    pub const fn encode<const LEN: usize>(mut input: &[u8]) -> [u8; LEN] {
+        let mut out = [0; LEN];
         let mut i = 0usize;
         let hex_char = b"0123456789ABCDEF";
         while let [c, ref next @ ..] = *input {
-            out[i]= hex_char[(c >> 4) as usize];
-            i +=1;
-            out[i]= hex_char[(c & 0xF) as usize];
-            i +=1;
+            out[i] = hex_char[(c >> 4) as usize];
+            i += 1;
+            out[i] = hex_char[(c & 0xF) as usize];
+            i += 1;
             input = next;
         }
         out
     }
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
 mod const_hex;
 use crate::const_hex::*;
 
-const DATA:&'static [u8]= {
-    const HEX:HexData = hex(b"niahcisa");
-    const LEN:usize = HEX.len;
+const DATA: &'static [u8] = {
+    const HEX: HexData = hex(b"niahcisa");
+    const LEN: usize = HEX.len;
     &HexData::encode::<LEN>(HEX.buf)
 };
 
-
 macro_rules! hex_encode {
-    ($buf:expr) => ({
-        const _HEX:HexData = hex($buf);
-        const _LEN:usize=_HEX.len;
-        const _DATA:&'static [u8]= &HexData::encode::<_LEN>(_HEX.buf);
+    ($buf:expr) => {{
+        const _HEX: HexData = hex($buf);
+        const _LEN: usize = _HEX.len;
+        const _DATA: &'static [u8] = &HexData::encode::<_LEN>(_HEX.buf);
         _DATA
-    });
+    }};
 }
 
-const MD:&'static [u8]=hex_encode!(b"aabxcsadf");
-const HD:&'static [u8]=hex_encode!(b"aabxcsadf");
+const MD: &'static [u8] = hex_encode!(b"aabxcsadf");
+const HD: &'static [u8] = hex_encode!(b"aabxcsadf");
 
 fn main() {
-    println!("{:?}",DATA);
+    println!("{:?}", DATA);
     unsafe {
         let x = std::str::from_utf8_unchecked(MD);
         println!("{:?}", x);
@@ -32,8 +31,8 @@ fn main() {
         println!("{:?}", x);
     }
 
-    let xx=hex_encode!("123123123".as_bytes());
-    println!("{:?}",xx);
-    let xx=hex_encode!("333333312".as_bytes());
-    println!("{:?}",xx);
+    let xx = hex_encode!("123123123".as_bytes());
+    println!("{:?}", xx);
+    let xx = hex_encode!("333333312".as_bytes());
+    println!("{:?}", xx);
 }
